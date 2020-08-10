8a9c34ecf6fc5c294be9595a82a5f39e1bba3cf5
david11yf29 <david11yf29@gmail.com>
2020-08-10 20:02:02 +0800

    updated

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -94,7 +95,25 @@ class BurgerBuilder extends React.Component {
     }
 
     purchaseContinueHandler = () => {
-        alert("You continue!");
+        // alert("You continue!");
+        // https://react-my-burger-692f4.firebaseio.com/
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'David',
+                address: {
+                    street: '1359 W Adams Blvd',
+                    zipCode: '90007',
+                    country: 'USA'
+                },
+                email: 'test@gmail.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('orders.json', order)
+            .then(res => console.log(res))
+            .catch(err => console.log(err));
     }
 
     render() {
