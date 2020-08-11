a41c62bd0ce41b2678db7ad90a07286d523c64ba
david11yf29 <david11yf29@gmail.com>
2020-08-11 22:21:51 +0800

    updated

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -12,6 +12,10 @@ class Checkout extends React.Component {
         }
     }
 
+    componentDidMount() {
+        
+    }
+
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
     }
